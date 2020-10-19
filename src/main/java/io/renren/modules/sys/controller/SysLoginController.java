ccbbc27a2a5dd84452981372f3d789892dcd4203
Kyrie Fang <2470781699@qq.com>
2020-10-19 15:04:44 +0800

    去掉验证码

--- src/main/java/io/renren/modules/sys/controller/SysLoginController.java
+++ src/main/java/io/renren/modules/sys/controller/SysLoginController.java
@@ -14,18 +14,12 @@ import io.renren.modules.sys.form.SysLoginForm;
 import io.renren.modules.sys.service.SysCaptchaService;
 import io.renren.modules.sys.service.SysUserService;
 import io.renren.modules.sys.service.SysUserTokenService;
-import org.apache.commons.io.IOUtils;
 import org.apache.shiro.crypto.hash.Sha256Hash;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.imageio.ImageIO;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Map;
 
@@ -46,28 +40,28 @@ public class SysLoginController extends AbstractController {
 	/**
 	 * 验证码
 	 */
-	@GetMapping("captcha.jpg")
-	public void captcha(HttpServletResponse response, String uuid)throws IOException {
-		response.setHeader("Cache-Control", "no-store, no-cache");
-		response.setContentType("image/jpeg");
-
-		//获取图片验证码
-		BufferedImage image = sysCaptchaService.getCaptcha(uuid);
-
-		ServletOutputStream out = response.getOutputStream();
-		ImageIO.write(image, "jpg", out);
-		IOUtils.closeQuietly(out);
-	}
+//	@GetMapping("captcha.jpg")
+//	public void captcha(HttpServletResponse response, String uuid)throws IOException {
+//		response.setHeader("Cache-Control", "no-store, no-cache");
+//		response.setContentType("image/jpeg");
+//
+//		//获取图片验证码
+//		BufferedImage image = sysCaptchaService.getCaptcha(uuid);
+//
+//		ServletOutputStream out = response.getOutputStream();
+//		ImageIO.write(image, "jpg", out);
+//		IOUtils.closeQuietly(out);
+//	}
 
 	/**
 	 * 登录
 	 */
 	@PostMapping("/sys/login")
 	public Map<String, Object> login(@RequestBody SysLoginForm form)throws IOException {
-		boolean captcha = sysCaptchaService.validate(form.getUuid(), form.getCaptcha());
-		if(!captcha){
-			return R.error("验证码不正确");
-		}
+//		boolean captcha = sysCaptchaService.validate(form.getUuid(), form.getCaptcha());
+//		if(!captcha){
+//			return R.error("验证码不正确");
+//		}
 
 		//用户信息
 		SysUserEntity user = sysUserService.queryByUserName(form.getUsername());
